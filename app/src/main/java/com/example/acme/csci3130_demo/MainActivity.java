6afef24ee59ebb8f3a105c7e6af4faeed6956e46
Ryan Amaral <ryan_amaral@live.com>
2018-01-18 13:52:13 -0400

    added text update feature

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.acme.csci3130_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void updateText(android.view.View v){
+        ((TextView)findViewById(R.id.text_view)).setText(
+                ((EditText)findViewById(R.id.text_input)).getText());
     }
 }
